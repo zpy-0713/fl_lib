8fe8caff724149cf2c8c9b8361aaa36c487c24f7
lollipopkit <lollipopkit@qq.com>
2024-06-05 11:22:44 +0800

    fix: cpu arch

--- lib/src/core/utils/platform/arch.dart
+++ lib/src/core/utils/platform/arch.dart
@@ -10,8 +10,26 @@ enum CpuArch {
   ;
 
   static const amd64Codes = ['x86_64', 'amd64'];
-  static const arm64Codes = ['arm64', 'aarch64', 'armv8', 'armv8a', 'arm64-v8a', 'arm64e'];
-  static const armCodes = ['arm', 'armv7', 'armv7a', 'armv7l', 'armv6', 'armv6l', 'armeabi', 'armeabi-v7a', 'armv5', 'armv5te'];
+  static const arm64Codes = [
+    'arm64',
+    'aarch64',
+    'armv8',
+    'armv8a',
+    'arm64-v8a',
+    'arm64e'
+  ];
+  static const armCodes = [
+    'arm',
+    'armv7',
+    'armv7a',
+    'armv7l',
+    'armv6',
+    'armv6l',
+    'armeabi',
+    'armeabi-v7a',
+    'armv5',
+    'armv5te'
+  ];
 
   static CpuArch get current {
     switch (Pfs.type) {
@@ -22,7 +40,9 @@ enum CpuArch {
           'ARM64' => CpuArch.arm64,
           _ => throw UnsupportedError('Unsupported CPU architecture: $cpu'),
         };
-      case Pfs.linux || Pfs.android || Pfs.macos || Pfs.fuchsia || Pfs.ios:
+      case Pfs.ios:
+        return CpuArch.arm64;
+      case Pfs.linux || Pfs.android || Pfs.macos || Pfs.fuchsia:
         final cpu = Process.runSync('uname', ['-m']);
         if (cpu.exitCode != 0) {
           throw Exception('Failed to run uname -m: ${cpu.stderr}');
