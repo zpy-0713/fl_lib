cdeb3a6e6baba512c06d5b86844aa45b4eb58689
lollipopkit🏳️‍⚧️ <10864310+lollipopkit@users.noreply.github.com>
2024-07-14 00:12:47 +0800

    new: `md5Sum`

--- lib/src/core/ext/uint8list.dart
+++ lib/src/core/ext/uint8list.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'dart:typed_data';
 
+import 'package:crypto/crypto.dart';
+
 extension FutureUint8ListX on Future<Uint8List> {
   Future<String> get string async => utf8.decode(await this);
   Future<ByteData> get byteData async => (await this).buffer.asByteData();
@@ -8,4 +10,8 @@ extension FutureUint8ListX on Future<Uint8List> {
 
 extension Uint8ListX on Uint8List {
   String get string => utf8.decode(this);
+
+  String get md5Sum => md5.convert(this).toString();
+
+  Future<ByteData> get byteData async => buffer.asByteData();
 }
