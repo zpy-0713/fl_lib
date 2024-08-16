1b75f1e6ab5792239e119c9031198d97c63ae01a
Noo6 <2746419228@qq.com>
2024-08-16 16:58:23 +0800

    add: `VirtualWindowFrame`

---
+++ lib/src/view/widget/virtual_window_frame.dart
@@ -0,0 +1,5 @@
+import 'package:window_manager/window_manager.dart' as wm;
+
+class VirtualWindowFrame extends wm.VirtualWindowFrame {
+  const VirtualWindowFrame({super.key, required super.child});
+}
