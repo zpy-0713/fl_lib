c9d3ee18b5fbf4e58d657e46987ccd700dfd6523
lollipopkit🏳️‍⚧️ <10864310+lollipopkit@users.noreply.github.com>
2024-09-12 10:27:37 +0800

    opt.: `AppLinksHandler

--- lib/src/core/app_link.dart
+++ lib/src/core/app_link.dart
@@ -1,44 +1,44 @@
 import 'package:fl_lib/fl_lib.dart';
 import 'package:flutter/widgets.dart';
 
-typedef AppLinkHandler = void Function(BuildContext ctx, Uri uri);
+typedef DeepLinkHandler = void Function(Uri uri, [BuildContext? context]);
 
-abstract final class AppLinksHandler {
-  static final _handlers = <AppLinkHandler>{_dispatchScheme};
+abstract final class DeepLinks {
+  static final _handlers = <DeepLinkHandler>{_dispatchScheme};
 
-  static void register(AppLinkHandler handler) {
+  static void register(DeepLinkHandler handler) {
     _handlers.add(handler);
   }
 
-  static void remove(AppLinkHandler handler) {
+  static void remove(DeepLinkHandler handler) {
     _handlers.remove(handler);
   }
 
-  static void process(BuildContext ctx, Uri uri) async {
+  static void process(Uri uri, [BuildContext? context]) async {
     for (final handler in _handlers) {
-      handler(ctx, uri);
+      handler(uri, context);
     }
   }
 
-  static void _dispatchScheme(BuildContext ctx, Uri uri) {
+  static void _dispatchScheme(Uri uri, [BuildContext? context]) {
     switch (uri.scheme) {
       case 'lpkt.cn':
-        _lpktcnHandler(ctx, uri);
+        _lpktcnHandler(uri, context);
         break;
     }
   }
 
-  static void _lpktcnHandler(BuildContext ctx, Uri uri) {
+  static void _lpktcnHandler(Uri uri, [BuildContext? context]) {
     switch (uri.host) {
       case 'general':
-        _generalHandler(ctx, uri);
+        _generalHandler(uri, context);
         break;
       default:
         Loggers.app.warning('[AppLinksHandler] Unknown host: ${uri.host}');
     }
   }
 
-  static void _generalHandler(BuildContext ctx, Uri uri) async {
+  static void _generalHandler(Uri uri, [BuildContext? context]) async {
     final params = uri.queryParameters;
 
     switch (uri.path) {
@@ -48,6 +48,8 @@ abstract final class AppLinksHandler {
         Apis.tokenProp.set(token);
         await Apis.userRefresh();
         break;
+      default:
+        Loggers.app.warning('[AppLinksHandler] Unknown path: ${uri.path}');
     }
   }
 }
