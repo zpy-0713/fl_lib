d88bc02e53423798ccaeca2be24220cdb0f08d0e
lollipopkit🏳️‍⚧️ <10864310+lollipopkit@users.noreply.github.com>
2024-09-16 10:09:29 +0800

    opt.: `Migration`

--- lib/src/core/migration.dart
+++ lib/src/core/migration.dart
@@ -2,16 +2,30 @@ import 'dart:async';
 
 import 'package:fl_lib/fl_lib.dart';
 
+/// -[old] The last version.
+/// -[now] The current version.
 typedef MigrationFn = FutureOr<void> Function(int old, int now);
 
-final class Migration {
-  static final fns = <MigrationFn>{};
+/// There are two occasions to run migration:
+/// - before runApp -> [fnsLaunch]
+/// - after home page loaded -> [fnsHome]
+abstract final class Migration {
+  /// before runApp
+  static final fnsLaunch = <MigrationFn>{};
 
+  /// after home page loaded
+  static final fnsHome = <MigrationFn>{};
+
+  /// Prop to store last version.
   static const lastVerProp = PrefProp<int>('last_ver');
 
   /// - [catchErr] If true, catch error and print it.
   /// Otherwise, throw it, and stop migration.
-  static Future<void> run(int now, {bool catchErr = true}) async {
+  static Future<void> run(
+    int now,
+    Set<MigrationFn> fns, {
+    bool catchErr = true,
+  }) async {
     final lastVer = lastVerProp.get() ?? 0;
     if (lastVer >= now) return;
 
