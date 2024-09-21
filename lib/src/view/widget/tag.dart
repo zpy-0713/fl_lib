9c69e55fd227428935de8de938c265ab5c099451
lollipopkit🏳️‍⚧️ <10864310+lollipopkit@users.noreply.github.com>
2024-09-21 22:32:19 +0800

    opt.: `TagSwitcher`

--- lib/src/view/widget/tag.dart
+++ lib/src/view/widget/tag.dart
@@ -4,9 +4,6 @@ import 'package:fl_lib/src/res/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
 
-const _kTagBtnHeight = 45.0;
-const kDefaultTag = '';
-
 /// A wrapped switcher for multiple tags.
 ///
 /// {@template tag-swicther-empty}
@@ -19,16 +16,33 @@ class TagSwitcher extends StatelessWidget implements PreferredSizeWidget {
   /// {@macro tag-swicther-empty}
   final String initTag;
 
+  /// If true, the tags will be wrapped in a [SingleChildScrollView].
+  /// Or in a [Wrap].
+  final bool singleLine;
+
+  /// If true, the tags will be reversed.
+  ///
+  /// Only works when [singleLine] is true.
+  final bool reversed;
+
+  final EdgeInsetsGeometry padding;
+
   const TagSwitcher({
     super.key,
     required this.tags,
     required this.onTagChanged,
     this.initTag = kDefaultTag,
+    this.singleLine = false,
+    this.reversed = false,
+    this.padding = const EdgeInsets.symmetric(horizontal: 13),
   });
+
+  static const kTagBtnHeight = 45.0;
+  static const kDefaultTag = '';
 
   @override
   Widget build(BuildContext context) {
-    final choice = tags.listenVal(
+    return tags.listenVal(
       (vals) {
         if (vals.isEmpty) return UIs.placeholder;
         final items = <String>[kDefaultTag, ...vals];
@@ -37,36 +51,53 @@ class TagSwitcher extends StatelessWidget implements PreferredSizeWidget {
           clearable: false,
           value: [initTag],
           builder: (state, _) {
-            return Wrap(
-              children: List<Widget>.generate(
-                items.length,
-                (index) {
-                  final item = items[index];
-                  return ChoiceChipX<String>(
-                    outPadding: const EdgeInsets.only(right: 5),
-                    padding: const EdgeInsets.symmetric(horizontal: 3),
-                    showCheckmark: false,
-                    label: item.isEmpty ? libL10n.all : '#$item',
-                    state: state,
-                    value: item,
-                    onSelected: (val, _) => onTagChanged(val),
-                  );
-                },
-              ),
-            );
+            return singleLine
+                ? _buildListView(items, state)
+                : _buildWrap(items, state);
           },
         );
       },
     );
-    return SingleChildScrollView(
-      padding: const EdgeInsets.symmetric(horizontal: 13),
+  }
+
+  Widget _buildItem(String item, ChoiceController<String> state) {
+    return ChoiceChipX<String>(
+      outPadding: const EdgeInsets.only(right: 5),
+      padding: const EdgeInsets.symmetric(horizontal: 3),
+      showCheckmark: false,
+      label: item.isEmpty ? libL10n.all : '#$item',
+      state: state,
+      value: item,
+      onSelected: (val, _) => onTagChanged(val),
+    );
+  }
+
+  Widget _buildWrap(List<String> items, ChoiceController<String> state) {
+    final children = List<Widget>.generate(
+      items.length,
+      (index) {
+        final item = items[index];
+        return _buildItem(item, state);
+      },
+    );
+    return Padding(padding: padding, child: Wrap(children: children));
+  }
+
+  Widget _buildListView(List<String> items, ChoiceController<String> state) {
+    return ListView.builder(
       scrollDirection: Axis.horizontal,
-      child: choice,
-    ).paddingOnly(bottom: 5);
+      itemCount: items.length,
+      reverse: reversed,
+      padding: padding,
+      itemBuilder: (context, index) {
+        final item = items[index];
+        return _buildItem(item, state);
+      },
+    );
   }
 
   @override
-  Size get preferredSize => const Size.fromHeight(_kTagBtnHeight);
+  Size get preferredSize => const Size.fromHeight(kTagBtnHeight);
 }
 
 final class TagTile extends StatelessWidget {
