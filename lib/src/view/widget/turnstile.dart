809aee90ce0d01704e33f7c8190ba25537a23c70
lollipopkit🏳️‍⚧️ <10864310+lollipopkit@users.noreply.github.com>
2025-05-14 15:30:41 +0800

    rm: turnstile

--- lib/src/view/widget/turnstile.dart
+++ lib/src/view/widget/turnstile.dart
@@ -1,19 +1,19 @@
-import 'package:cloudflare_turnstile/cloudflare_turnstile.dart';
+// import 'package:cloudflare_turnstile/cloudflare_turnstile.dart';
 
-final class Turnstile extends CloudflareTurnstile {
-  static const mySiteKey = String.fromEnvironment('TURNSTILE_SITE_KEY');
-  static const verifyEndpoint = 'https://api.lpkt.cn/auth/turnstile';
+// final class Turnstile extends CloudflareTurnstile {
+//   static const mySiteKey = String.fromEnvironment('TURNSTILE_SITE_KEY');
+//   static const verifyEndpoint = 'https://api.lpkt.cn/auth/turnstile';
 
-  Turnstile({
-    super.siteKey = mySiteKey,
-    super.key,
-    super.onError,
-    required super.onTokenReceived,
-    super.baseUrl = 'https://api.lpkt.cn',
-    super.onTokenExpired,
-    super.action,
-    super.cData,
-    super.controller,
-    super.options,
-  });
-}
+//   Turnstile({
+//     super.siteKey = mySiteKey,
+//     super.key,
+//     super.onError,
+//     required super.onTokenReceived,
+//     super.baseUrl = 'https://api.lpkt.cn',
+//     super.onTokenExpired,
+//     super.action,
+//     super.cData,
+//     super.controller,
+//     super.options,
+//   });
+// }
